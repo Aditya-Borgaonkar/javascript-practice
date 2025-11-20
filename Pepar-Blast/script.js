2855940e4c4336eac5a6d24dd885b640dc3c577c
Aditya Borgaonkar <adityaborgaonkar4@gmail.com>
2025-11-20 14:17:47 +0530

    Add randon width & hight

--- Pepar-Blast/script.js
+++ Pepar-Blast/script.js
@@ -8,17 +8,19 @@ btn.addEventListener('click',function(){
     let x=Math.random()*90;
     let y=Math.random()*90;
     let rot=Math.random()*361;
+    let he=Math.random()*90;
+    let wi=Math.random()*90;
     let c1=Math.floor(Math.random()*256)
     let c2=Math.floor(Math.random()*256)
     let c3=Math.floor(Math.random()*256)
 
 
-    div.style.height='80px'
-    div.style.width='80px'
-    div.style.backgroundColor='red'
+    div.style.height= he+'px'
+    div.style.width= wi+'px'
     div.style.position='absolute'
     div.style.left= x+'%'
     div.style.top= y+'%'
+    div.style.rotate=rot+'deg'
     div.style.backgroundColor= `rgb(${c1},${c2},${c3})`
 
 
